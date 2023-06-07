e6f989adfb4d474f51a792799763f0019da1a760
SSuitec99Double <104209435+Suitec9@users.noreply.github.com>
2023-06-07 08:25:47 +0200

    #Commit

---
+++ hardhat-rerun/scripts/deploy.js
@@ -0,0 +1,35 @@
+require("dotenv").config({ path: ".env" });
+const { ethers } = require("hardhat");
+
+async function main() {
+  
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
+  */
+  const ConnectlistContract = await ethers.getContractFactory(
+    "Connectlist"
+  );
+
+  // deploy the contract
+  const deployedConnectlistContract = await ConnectlistContract.deploy(100);
+    // 100 is the Maximum number for whitelisted addresses allowed
+
+
+    // Wait for it to finish deploying
+    await deployedConnectlistContract.deployed();
+
+  // print the address of the deployed contract
+  console.log(
+    "Connectlist Contract",
+    deployedConnectlistContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
